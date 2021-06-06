b1a53296c39ab4acc05d32b4439a740ed2d33a99
Fe1c <tguyofawesome@gmail.com>
2021-06-06 16:06:15 -0700

    Wrote test in unittest

--- test_calculator.py
+++ test_calculator.py
@@ -2,4 +2,20 @@ import unittest
 import calculator
 
 
-class TestCalculatorApp:
+class TestCase(unittest.TestCase):
+
+    def test_add_1(self):
+        self.assertEqual(calculator.add(1, 2), 3)
+
+    def test_subtract_1(self):
+        self.assertEqual(calculator.subtract(1, 1), 0)
+
+    def test_mul_1(self):
+        self.assertEqual(calculator.mul(1, 4), 4)
+
+    def test_div_1(self):
+        self.assertEqual(calculator.division(24, 8), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
